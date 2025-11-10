1358eb159ba9615de7b6ebb1169a52989c04a857
Dragonwinnn1 <dreamschase202@gmail.com>
2025-11-10 15:03:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Data produk (sesuaikan dengan nama file gambar Anda!)
 const daftarProduk = [
-    { id: 1, nama: "Urban Mischief", harga: 249000, gambar: "https://lh3.googleusercontent.com/gg/AIJ2gl8LV_x7Cjd3cJy8Qjb6WAiNdZOcY_vAgq4Dy_AW46b0qc3KknQAIOHGw0U5yXDPkSTH5O1RuvK-MnkJ-YIH_D-RkLrZPiocBHk3s0ySt3ywB9YApunsDKOK8XwmXVZxA-CCuDJvhNlQn91k86cY1eNkaPgNrioQhwt3FoiHX7to-VnlhNgty-ycVx7kM12pSJ72C-TKqOc3axPUim0aQVAZayv2kF4xZQiSp5eN11Tr9FWeOZXvt3KmB82arptfcX6pMx_5y9P9YzX1aJCEkTB1zagagAlTvw=d" },
-    { id: 2, nama: "Black & White Anarchy", harga: 489000, gambar: "https://lh3.googleusercontent.com/gg/AIJ2gl_n_6kRWWfoz0gxrDWsnCWY9VVBRLii5j1jD5o5l7JOx-J3qJURYhK0z9gkD0LeOGUnfSDerDC18MOZCwSFM8X2AI6kh7SECJ3u4_lzAr4ErJ5x4syLCb2zawu2K_s3uS_dgMnYfxEp6kYbfhZOZwFLXzOuJQZDyFPTYaA-DtgOz40WVHyCuCIvFpjGeGfDw4mon9gofsUCNYhGluuDa9agh5uZN6-HZrr6aEij885Q8DY3lM9jwxeQWzpTC3gavwJz1vMU7s_pGNB1BAVLbJjDbE0XW4049hc=d" },
+    { id: 1, nama: "Urban Mischief", harga: 249000, gambar: "https://github.com/Dragonwinnn1/PAWSofficialSTORE/blob/main/WSA.jpg?raw=true" },
+    { id: 2, nama: "Black & White Anarchy", harga: 489000, gambar: "https://github.com/Dragonwinnn1/PAWSofficialSTORE/blob/main/WSA1.jpg?raw=true" },
     { id: 3, nama: "CARGO PANTS BLACK", harga: 399000, gambar: "distro-celana-03.jpg" },
     { id: 4, nama: "WINDBREAKER V2", harga: 650000, gambar: "distro-jaket-04.jpg" },
     { id: 5, nama: "SNAPBACK ORIGINAL", harga: 199000, gambar: "distro-topi-05.jpg" },
@@ -64,3 +64,4 @@ document.addEventListener('DOMContentLoaded', () => {
     updateKeranjangUI();
 
 });
+
